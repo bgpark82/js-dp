128fff3c20bc135b9bbfb42fcffa58b3ba4d391c
bgpark <bgpark@citus.co.kr>
2020-04-03 11:41:58 +0900

    mvc 패턴 완료

--- mvc/0.js
+++ mvc/0.js
@@ -1,7 +1,19 @@
+const Singleton = class extends WeakMap{
+    has(){err()};
+    get(){err()};
+    set(){err()};
+    getInstance(v){
+        if(!super.has(v.constructor)) super.set(v.constructor, v);
+        return super.get(v.constructor);
+    }    
+}
+
+const singleton = new Singleton;
+
 const Model = class extends Set{
     constructor(isSingleton){
         super();
-        if(isSingleton) return isSingleton.getInstance(this);
+        if(isSingleton) return singleton.getInstance(this);
     }
 
     add() {err();};
@@ -15,7 +27,7 @@ const Model = class extends Set{
         super.delete(ctrl);
     }
     notify(){
-        this.forEach(model => model.listen(this));
+        this.forEach(ctrl => ctrl.listen(this));
     }
 }
 
@@ -35,7 +47,7 @@ const HomeModel = class extends Model {
     constructor(isSingleton){
         super(isSingleton);
         if(!this._list) {
-            _list :[
+            this._list = [
                 new HomeDetailModel(1, 'todo1','memo1'),
                 new HomeDetailModel(2, 'todo2','memo2'),
                 new HomeDetailModel(3, 'todo3','memo3'),
@@ -45,8 +57,16 @@ const HomeModel = class extends Model {
             ]
         }
     }
+    get list() { return [...this._list]}
 
-    add(...model){this._list.push(...model)}
+    add(...model){
+        this._list.push(...model)
+    }
+    get(id){
+        let result;
+        if(!this._list.some(model => model.id == id ? (result = model) : false)) err();
+        return result;
+    }
     remove(id){
         const {_list:list} = this;
         if(!list.some((model,i) =>{
@@ -54,8 +74,115 @@ const HomeModel = class extends Model {
                 list.splice(i,1);
                 return true;
             }
-        }))
+        })) err();
         this.notify();
     }
-
 }
+
+const View = class {
+    constructor(controller, isSingleton = false){
+        this._controller = controller;
+        if(isSingleton) return singleton.getInstance(this);
+    }
+    render(model = null) { override() }
+}
+
+const HomeBaseView = class extends View {
+    constructor(controller, isSingleton){
+        super(controller, isSingleton);
+    }
+    render(model, _=type(model, HomeModel)) {
+        const {_controller:ctrl } = this;
+        return append(el('ul'), ...model.list.map(model => append(
+            el('li'),
+            el('a','innerHTML',model.title, 'addEventListener',['click',_=>ctrl.$detail(model.id)]),
+            el('button','innerHTML','x','addEventListener',['click',_=>ctrl.$remove(model.id)])
+        )))
+    }
+}
+
+const HomeDetailView = class extends View {
+    constructor(controller, isSingleton){
+        super(controller, isSingleton);
+    }
+    render(model, _=type(model, HomeDetailModel)){
+        const {_controller:ctrl} = this;
+        return append(el('section'),
+            el('h2','innerHTML',model.title),
+            el('p','innerHTML',model.memo),
+            el('button','innerHTML','delete','addEventListener',['click',_=>ctrl.$removeDetail(medel.id)]),
+            el('button','innerHTML','list','addEventListener',['click',_=>ctrl.$list()])
+        )
+    }
+}
+
+const Controller = class{
+    constructor(isSingleton){
+        if(isSingleton) return singleton.getInstance(this);
+    }
+    listen(model){}
+}
+
+const Home = class extends Controller {
+    constructor(isSingleton){
+        super(isSingleton);
+    }
+    base(){
+        const view = new HomeBaseView(this, true);
+        const model = new HomeModel(true);
+        return view.render(model);
+    }
+    detail(id){
+        const view = new HomeDetailView(this, true);
+        const model = new HomeModel(true);
+        return view.render(model.get(id));
+    }
+
+    $detail(id){
+        app.route('home:detail',id);
+    }
+    $remove(id){
+        const model = new HomeModel(true);
+        model.remove(id);
+        this.$list();
+    }
+    $list(){
+        app.route('home')
+    }
+    $removeDetail(id){
+        this.$remove(id);
+        this.$list();
+    }
+}
+
+const App = class {
+    constructor(parent){
+        this._parent = parent;
+        this._table = new Map();
+    }
+    add(route, controller){
+        route = route.split(':')
+        this._table.set(route[0],controller);  // home : ctrl
+        (route[1] || '').split(',').concat('base').forEach(v =>{ // [detail, base]
+            this._table.set(`${route[0]}:v`, controller); // [detail:v : ctrl][base:v : ctrl]
+        })
+    }
+    route(path, ...arg){
+        const [k, action = 'base'] = path.split(':') // home
+        if(!this._table.has(k)) return;
+        const controller = this._table.get(k)(); // home에 해당하는 controller 찾기
+        append(el(sel(this._parent), 'innerHTML',''), controller[action](...arg)); // controller(detail)(...arg)
+
+    }
+}
+
+const append = (parent, ...children) => {
+    children.forEach(child => {
+        parent.appendChild(child);
+    })
+    return parent
+}
+
+const app = new App('#stage');
+app.add('home:detail',_=>new Home(true));
+app.route('home');
