bd398d80fc32af4cd93a0802c37bf9297697037e
bgpark <bgpark@citus.co.kr>
2020-04-08 11:31:23 +0900

    mvvc 아웃라인 작성

--- mvvp/1.js
+++ mvvp/1.js
@@ -1,77 +1,140 @@
 const Observer = class {
-    observe(subject) {override()}
+    observe(v) {override()}
 }
 
-const ViewModelObserver = class extends Observer {
-    constructor(viewModel){
-        super();
-        this._viewModel = viewModel;
-    }
-    observe(model){ this._viewModel.listen(model)}
-}
-
-const Subject = class extends Set{
+const Subject = class extends Set {
     add(observer){
         super.add(observer)
         return this;
     }
-    delete(observer){
-        super.delete(observer)
-    }
-    has(observer){
-        return super.has(observer)
-    }
     notify(...arg){
-        this.forEach(observer => arg.length ? observer.observe(...arg): observer.observe(this));
+        this.forEach(observer => arg.length ? observer.observe(...arg) : observer.observe(this))
     }
 }
 
-// {vm, observer}
-const ViewModel = class extends Subject {
+
+const Model = class extends Subject {
     constructor(isSingleton) {
-        super()
-        const vm = isSingleton ? singleton.getInstance(this) : this;
-        const _observer = new ViewModelObserver(vm);
-        prop(vm,{_observer});
+        super();
+        if(isSingleton) return singleton.getInstance(this)
     }
-    get observer() {return this._observer}
-    listen(model){}
 }
 
-const ListVM = class extends ViewModel{
-    constructor(isSingleton) {
+const HomeModel = class extends Model{
+    constructor(isSingleton){
         super(isSingleton)
+        if(!this._list){
+            prop(this, {_list : [
+                new HomeDetailModel(1, 'todo1','memo1'),
+                new HomeDetailModel(2, 'todo2','memo2'),
+                new HomeDetailModel(3, 'todo3','memo3'),
+            ]})
+        }
+    }
+    get list() {return this._list}
+}
+
+const HomeDetailModel = class extends Model {
+    constructor(id, title, memo) {
+        super();
+        this._id = id;
+        this.edit(title, memo)
+    }
+    edit(title, memo){
+        this._title = title,
+        this._memo = memo
+        this.notify();
+    }
+    get title() {return this._title}
+}
+
+const View = class extends Observer{
+    constructor(tag, isSingleton = false) {
+        super();
+        return prop(isSingleton ? singleton.getInstance(this) : this, {_tag:tag})
+    }
+
+    get tag() { return this._tag}
+    get viewModel() {return this._viewModel}
+    set viewModel(_viewModel){
+        prop(this, {_viewModel})
+        _viewModel.add(this);
+    }
+
+    observe(...arg){this.render(...arg)}
+    render(){override()}
+}
+
+const HomeBaseView = class extends View{
+    constructor(isSinglton){
+        super(el('ul'),isSinglton)
+    }
+    render(model){
+        const {viewModel, tag} = this;
+        append(el(tag,'innerHTML',''), ...model.map(m => append(
+            el('li'),
+            el('a','innerHTML',m.title)
+        )))
+    }
+}
+
+
+
+// {set}
+const ViewModelObserver = class extends Observer{
+    constructor(viewModel) {
+        super();
+        this._viewModel = viewModel;
+    }
+    observe(model){
+        this._viewModel.listen(model)
+    }
+}
+
+
+// {set(), viewmodelobserver}
+const ViewModel = class extends Subject{
+    constructor(isSinglton){
+        super();
+        const target = isSinglton ? singleton.getInstance(this) : this;
+        prop(target, {_observer: new ViewModelObserver(target)})
+        return target; 
+    }
+    get observer(){return this._observer}
+
+}
+const ListVM = class extends ViewModel{
+    constructor(isSinglton){
+        super(isSinglton);
     }
     base(){
-        const model = new HomeModel(true);
-        model.add(this.observer)
-        model.notify();
+        const model = new HomeModel(true);      // model 생성
+        model.add(this.observer)                // model에 observer 등록    
+        model.notify();                         // notify로 observer 실행
     }
     listen(model){
-        this.nofity(model.list)
+        this.notify(model.list)
     }
 }
 
-const App = class {
+const App = class{
     constructor(parent) {
-        this._parent = parent;
-        this._table = new Map;
+        this._parent= parent;
+        this._table= new Map; 
     }
-
     add(path, vm, view){
-        this._table.set(path,[vm, view]);
+        this._table.set(path,[vm,view])
     }
-    route(path, ...arg){
-        const [p, action = 'base'] = path.split(':');
+    route(path,...arg){
+        const [p, action='base'] = path.split(':');
         const [vm, view] = this._table.get(p).map(f => f());
         view.viewModel = vm;
-        vm[action](...arg)
-        append(el(sel(this._parent),'innerHTML',''), view.view);
+        vm[action](...arg);
+        append(el(sel(this._parent),'innerHTML',''), view.tag);
     }
 }
 
-
 const app = new App("#stage");
-app.add('list',_=> new ListVM(true),_=> new HomeBaseView(true))
-app.add('detail',_=> new DetailVM(true),_=>new HomeDetailView(true))
+app.add('list',_=>new ListVM(true),_=>new HomeBaseView(true));
+app.add('detail',_=>new DetailVM(true),_=>new HomeDetailView(true));
 app.route('list');
